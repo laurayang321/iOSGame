b8cea2f3bd0f8efeafc297d0370436a45f85a295
FeuerSchweinaBar <runejunior@gmail.com>
2017-02-10 16:43:23 -0800

    initial commit

---
+++ Toxote/Toxote/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  SpriteKitSimpleGame
+//
+//  Created by Laura Yang on 2017-01-27.
+//  Copyright © 2017 Laura Yang. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
